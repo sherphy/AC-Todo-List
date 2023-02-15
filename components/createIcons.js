34e4db7d1f0747bb2fb4ca47fb29b544cc774602
sherphy <sherlynn.phyllisa@u.nus.edu>
2023-02-15 21:38:38 +0800

    trying to fix icon

--- components/createIcons.js
+++ components/createIcons.js
@@ -1,33 +1,33 @@
 export default function createIcons(projectIcon,counter) {
     counter = counter%10;
     if (counter === 0) {
-        projectIcon.src = '../utils/icon0.png';
+        projectIcon.src = './utils/icon0.png';
     }
     else if (counter === 1) {
         projectIcon.src = './utils/icon1.png';
     }
     else if (counter === 2) {
-        projectIcon.src = '../utils/icon2.png';
+        projectIcon.src = './utils/icon2.png';
     }
     else if (counter === 3) {
-        projectIcon.src = '../utils/icon3.png';
+        projectIcon.src = './utils/icon3.png';
     }
     else if (counter === 4) {
-        projectIcon.src = '../utils/icon4.png';
+        projectIcon.src = './utils/icon4.png';
     }
     else if (counter === 5) {
-        projectIcon.src = '../utils/icon5.png';
+        projectIcon.src = './utils/icon5.png';
     }
     else if (counter === 6) {
-        projectIcon.src = '../utils/icon6.png';
+        projectIcon.src = './utils/icon6.png';
     }
     else if (counter === 7) {
-        projectIcon.src = '../utils/icon7.png';
+        projectIcon.src = './utils/icon7.png';
     }
     else if (counter === 8) {
-        projectIcon.src = '../utils/icon8.png';
+        projectIcon.src = './utils/icon8.png';
     }
     else {
-        projectIcon.src = '../utils/icon9.png';
+        projectIcon.src = './utils/icon9.png';
     }
 }

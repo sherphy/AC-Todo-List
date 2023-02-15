e73b51c41d9d6a06874d1e19261f1db8cef5bf3f
sherphy <sherlynn.phyllisa@u.nus.edu>
2023-02-15 21:03:16 +0800

    trying relative url

--- components/createIcons.js
+++ components/createIcons.js
@@ -4,7 +4,7 @@ export default function createIcons(projectIcon,counter) {
         projectIcon.src = '../utils/icon0.png';
     }
     else if (counter === 1) {
-        projectIcon.src = '../utils/icon1.png';
+        projectIcon.src = '/utils/icon1.png';
     }
     else if (counter === 2) {
         projectIcon.src = '../utils/icon2.png';
